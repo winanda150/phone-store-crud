7043261b7f46e8dd2cee5778240cac0f04adf693
winanda150 <winandabekul150@gmail.com>
2026-07-06 11:59:25 +0800

    Fix masalah di slidebar kiri

--- laporan_penjualan.php
+++ laporan_penjualan.php
@@ -87,30 +87,47 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
             <a href="dashboard.php" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i><p>Dashboard</p>
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
             </a>
           </li>
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-table"></i><p>Data Master<i class="fas fa-angle-left right"></i></p>
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Data Master
+                <i class="fas fa-angle-left right"></i>
+              </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="user.php" class="nav-link"><i class="far fa-circle nav-icon"></i><p>Data User</p></a>
+                <a href="user.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data User</p>
+                </a>
               </li>
             </ul>
           </li>
           <li class="nav-header">TRANSAKSI</li>
           <li class="nav-item">
-            <a href="penjualan.php" class="nav-link"><i class="nav-icon far fa-calendar-alt"></i><p>Penjualan</p></a>
+            <a href="penjualan.php" class="nav-link">
+              <i class="nav-icon far fa-calendar-alt"></i>
+              <p>Penjualan</p>
+            </a>
           </li>
           <li class="nav-item">
-            <a href="pembelian.php" class="nav-link"><i class="nav-icon far fa-image"></i><p>Pembelian</p></a>
+            <a href="pembelian.php" class="nav-link">
+              <i class="nav-icon far fa-image"></i>
+              <p>Pembelian</p>
+            </a>
           </li>
           <li class="nav-header">LAPORAN</li>
           <li class="nav-item">
             <a href="laporan_penjualan.php" class="nav-link active">
-              <i class="nav-icon fas fa-file"></i><p>Laporan Penjualan</p>
+              <i class="nav-icon fas fa-file"></i>
+              <p>Laporan Penjualan</p>
             </a>
           </li>
         </ul>
